453f03720ea7df3a3c03dd15cb65470ff4ff5615
calebthewood <calebwood.cs@gmail.com>
2023-01-06 17:00:58 -0800

    Rename Draggable

--- react-native/App.js
+++ react-native/App.js
@@ -1,14 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
-import Draggable from './Draggable';
+import { View, StyleSheet } from 'react-native';
+import SpringToOrigin from './SpringToOrigin';
+import DragNDrop from './DragNDrop';
 
 export default function App() {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "skyblue"
+    }
+  });
 
   return (
     <View style={{ flex: 1 }}>
-      <Draggable />
+      <View style={styles.container}>
+        <SpringToOrigin />
+        <DragNDrop />
+      </View>
       <StatusBar style="auto" hidden={true} />
-    </View>
+    </View >
   );
 }
 
