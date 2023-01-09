b388bd59e775168673a43702b6b88c205c77e2f8
calebthewood <calebwood.cs@gmail.com>
2023-01-08 19:00:09 -0800

    Home screen layout - WIP

--- react-native/App.js
+++ react-native/App.js
@@ -1,30 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { View, StyleSheet } from 'react-native';
-import SpringToOrigin from './SpringToOrigin';
-import DragNDrop from './DragNDrop';
-import SnapTo from './SnapTo';
+import { View, StyleSheet, SafeAreaView, Text } from 'react-native';
+import { RotatingView } from './src/components/RotatingView';
+import { BigButton } from './src/containers/BigButton';
 
 export default function App() {
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: "skyblue"
-    }
-  });
 
   return (
-    <View style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <SpringToOrigin />
-        <DragNDrop />
-        <SnapTo />
-        <SnapTo />
-        <SnapTo />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.buttonsCol}>
+        <BigButton style={styles.getMoneyBtn} btnLabel={"Get Money"} />
+        <BigButton style={styles.useMoneyBtn} btnLabel={"Use Money"} />
+      </View>
+      <View style={styles.mainCol}>
+        <Text>Main Display</Text>
+      </View>
+      <View style={styles.walletCol}>
+        <Text>Wallet</Text>
       </View>
       <StatusBar style="auto" hidden={true} />
-    </View >
+    </SafeAreaView >
   );
 }
 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#b8e994", //green
+    flexDirection: "row"
+  },
+  buttonsCol: {
+    flex: 1,
+    backgroundColor: "#82ccdd", //blue
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+  },
+  getMoneyBtn: {
+
+  },
+  useMoneyBtn: {
+
+
+  },
+  mainCol: {
+    flex: 4,
+    backgroundColor: "#6a89cc" //livid
+  },
+  walletCol: {
+    flex: 1,
+    backgroundColor: "#f8c291" //Melon
+  }
+});
