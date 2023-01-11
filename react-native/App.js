24ff54bf3c76601ab6595787c5951b5401206041
calebthewood <calebwood.cs@gmail.com>
2023-01-11 11:12:19 -0800

    Hide navigation header

--- react-native/App.js
+++ react-native/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from './src/containers/HomeScreen';
@@ -9,10 +8,9 @@ function App() {
   const image = { uri: "assets/landscape/demo-homescreen.png" };
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="home" component={HomeScreen} />
+      <Stack.Navigator style={{display: 'hidden'}}>
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
         {/* <Stack.Screen name="make-money" component={MakeMoney} /> */}
-        <StatusBar style="auto" hidden={true} />
       </Stack.Navigator>
     </NavigationContainer>
   );
